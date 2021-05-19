ea0b7ec6006b13f55df948cac9008cc618239441
mymichu <meyer_michel@outlook.com>
2021-05-19 13:03:35 +0000

    remove sdk and add blinky stm

--- debugServer.sh
+++ debugServer.sh
@@ -1,3 +1,1 @@
-#openocd -c "gdb_port 3333" -c "telnet_port 4444" -f interface/jlink.cfg -f board/nordic_nrf52_dk.cfg -c "bindto 172.17.0.1" -c 'init; reset init; halt"
 JLinkRemoteServer
-#JLinkGDBServerCLExe -select USB -device nRF52840_xxAA -endian little -if SWD -speed auto -noir -noLocalhostOnly --help
