486501d19add632885cba044dec431d05401d9d6
Stefan Eicher <stefan.eicher@ypsomed.com>
2021-05-21 14:48:01 +0200

    use os.path.dirname

--- demo/conanfile.py
+++ demo/conanfile.py
@@ -1,3 +1,4 @@
+import os
 from conans import ConanFile, CMake
 
 
@@ -26,7 +27,7 @@ class DemoConan(ConanFile):
     def build(self):
         cmake = CMake(self)
         cmake.configure(source_folder="src")
-        self.run("/app/build-wrapper/build-wrapper-linux-x86-64 --out-dir bw-output cmake --build .")
+        self.run(f"/{os.path.dirname(__file__)}/../build-wrapper/build-wrapper-linux-x86-64 --out-dir bw-output cmake --build .")
         
 
     def package(self):
