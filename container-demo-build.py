67d9682d5839b6d5c030b88059bb12a33354e1a6
Stefan Eicher <stefan.eicher@ypsomed.com>
2021-05-21 12:17:07 +0000

    run sonar

---
+++ container-demo-build.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import os
+import docker
+
+
+def run_build(docker_image, container_command):
+    client = docker.from_env()
+    current_path = os.getcwd()
+    try:
+        retVal = client.containers.run(image=docker_image, command=container_command, remove=True,
+                                       working_dir="/app", volumes={current_path: {'bind': '/app', 'mode': 'rw'}})
+        retVal = retVal.decode('utf-8')
+        print(f'{retVal}')
+        print("SUCCESS")
+    except docker.errors.ContainerError as exc:
+        exc = exc.stderr.decode('utf-8')
+        print(f'Failed to run container:')
+        print(exc)
+        exit(1)
+
+
+if __name__ == "__main__":
+    bash_command = "setup; ./buildDemo.sh"
+    command = f"/bin/bash -c '{bash_command}'"
+    run_build("disroop/embedded-hipster:0.4.0", command)
---
+++ demo/conanfile.py
@@ -0,0 +1,41 @@
+from conans import ConanFile, CMake
+
+
+class DemoConan(ConanFile):
+    name = "demo"
+    version = "1.0"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Demo here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "cmake"
+    exports_sources = "src/*"
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        self.run("/workspace/build-wrapper/build-wrapper-linux-x86-64 --out-dir bw-output cmake --build .")
+        
+
+    def package(self):
+        self.copy("*.h", dst="include", src="src")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["demo"]
