c3fbd8f7b4192d75b662793d514a6dd0c6969add
Stefan Eicher <stefan.eicher@ypsomed.com>
2021-05-25 01:13:09 +0200

    update demo

---
+++ demo/test/test_demo.cpp
@@ -0,0 +1,15 @@
+
+extern "C"
+{
+    // #include "demo.h"
+}
+
+
+#include <gtest/gtest.h>
+
+TEST(TestDemo, Test1)
+{
+    // int ret =demo_doit();
+    // EXPECT_EQ(ret, 123);
+    EXPECT_EQ(123, 123);
+}
