66100546eb772f64a45fff7c938fb80855810005
Stefan Eicher <stefan.eicher@ypsomed.com>
2021-06-09 23:47:18 +0200

    fix FindCoverage.cmake

--- blinky/cmake/FindCoverage.cmake
+++ blinky/cmake/FindCoverage.cmake
@@ -1,13 +1,13 @@
-macro(target_configure_coverage target)
+macro(target_configure_coverage arg)
     if (NOT CMAKE_CROSSCOMPILING)
         message(info "Configured coverage for target ${arg}")
-        target_compile_options(${target}
+        target_compile_options(${arg}
                 PUBLIC
                 "-O0"
                 "-fprofile-arcs"
                 "-ftest-coverage"
                 )
-        target_link_libraries(${target} PUBLIC gcov)
-        target_link_options(${target} PUBLIC --coverage)
+        target_link_libraries(${arg} PUBLIC gcov)
+        target_link_options(${arg} PUBLIC --coverage)
     endif ()
 endmacro()
