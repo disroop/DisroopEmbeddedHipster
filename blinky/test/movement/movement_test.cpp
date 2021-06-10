d96d062e7e10a280237a1e506a8e74b53debf760
Michel Meyer <meyer_michel@outlook.com>
2021-06-10 15:53:22 +0200

    add movement utest

---
+++ blinky/test/movement/movement_test.cpp
@@ -0,0 +1,43 @@
+extern "C" {
+#include "movement.h"
+}
+#include "gtest/gtest.h"
+#include "internal/fake_gyro.hpp"
+using namespace ::testing;
+
+// Wrapper function uses a global to remember the object:
+static std::unique_ptr<FakeGyro> pfakeGyro;
+static void fake_gyro_wrapper(float* xyz) { pfakeGyro->gyro_xyz(xyz); }
+
+class UT_Movement : public testing::Test {
+   public:
+    void SetUp() override {
+        pfakeGyro = std::unique_ptr<FakeGyro>(new FakeGyro());
+    }
+
+    void TearDown() override {}
+};
+
+TEST_F(UT_Movement, run_over_threshold) {
+    pfakeGyro->set_gyro(100, 200, 1001);
+    movement_init(&fake_gyro_wrapper);
+    movement_reset();
+    movement_run();
+    EXPECT_TRUE(movement_has_rotated());
+}
+
+TEST_F(UT_Movement, run_over_threshold) {
+    pfakeGyro->set_gyro(100, 200, 1001);
+    movement_init(&fake_gyro_wrapper);
+    movement_reset();
+    movement_run();
+    EXPECT_TRUE(movement_has_rotated());
+}
+
+TEST_F(UT_Movement, run_over_threshold) {
+    pfakeGyro->set_gyro(100, 200, 1001);
+    movement_init(&fake_gyro_wrapper);
+    movement_reset();
+    movement_run();
+    EXPECT_TRUE(movement_has_rotated());
+}
