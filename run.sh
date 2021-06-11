8c823b18fb7726f917477b1b917fe136bd77037a
Stefan Eicher <stefan.eicher@gmail.com>
2021-06-11 13:03:50 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit
- function runWithDocker() {
-     if [ "$(docker ps -q -f name=forty-jekyll-theme-1)" ]; then
-       echo exists
-       docker stop forty-jekyll-theme-1 || true && docker rm forty-jekyll-theme-1 || true
-     else
-       echo else
-     fi
-     open http://localhost:4001/DisroopEmbeddedHipster
-     docker run --rm --name forty-jekyll-theme-1 --volume="$(pwd):/srv/jekyll" -p 4001:4000 -it  jekyll/jekyll:latest  jekyll serve --watch --drafts
- }
-
-function run() {
-    #oder
-  open http://localhost:4000
-  jekyll serve --watch --drafts --trace
+function runWithDocker() {
+  if [ "$(docker ps -q -f name=forty-jekyll-theme-1)" ]; then
+    echo exists
+    docker stop forty-jekyll-theme-1 || true && docker rm forty-jekyll-theme-1 || true
+  else
+    echo else
+  fi
+  open http://localhost:4002/DisroopEmbeddedHipster/
+  docker run --rm --volume="$PWD:/srv/jekyll" -p 4002:4000 -it jekyll/jekyll:latest jekyll serve --watch --drafts
 }
 
+function run() {
+  #oder
+  open http://localhost:4000/DisroopEmbeddedHipster
+  jekyll serve --watch --drafts --trace
+}
 
 #run
 runWithDocker
