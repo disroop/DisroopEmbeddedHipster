d6a4f8525d0ed780fce495bfac190382bcbdcc20
Stefan Eicher <stefan.eicher@gmail.com>
2021-06-28 01:22:59 +0200

    update doc

--- tasks/doc.py
+++ tasks/doc.py
@@ -4,6 +4,12 @@ from invoke import task
 @task(aliases=['g'])
 def gen(c):
     """generate files used in the documentation"""
+    # project cli commands
     c.run("inv -l --list-depth 1 > ./doc/project_commands.txt")
     c.run("inv -l > ./doc/all_commands.txt")
     c.run("inv -l code> ./doc/vscode_commands.txt")
+
+    # conan graphs
+    c.run("conan info ./blinky --graph doc/conan_graph.dot")
+    # cmakegraphs
+    # todo
