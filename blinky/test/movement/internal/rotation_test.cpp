29f85f8cf45bc8abad38b5fc697eaef1c60847cf
Michel Meyer <meyer_michel@outlook.com>
2021-07-04 09:21:17 +0000

    replace fake_gyro with lambda

--- blinky/test/movement/internal/rotation_test.cpp
+++ blinky/test/movement/internal/rotation_test.cpp
@@ -1,25 +1,15 @@
 extern "C" {
 #include "internal/rotation.h"
 }
-#include "fake_gyro.hpp"
 #include "gtest/gtest.h"
 
-// Wrapper function uses a global to remember the object:
-static std::unique_ptr<FakeGyro> pfakeGyro;
-static void fake_gyro_wrapper(float* xyz) { pfakeGyro->gyro_xyz(xyz); }
-
-class UT_Rotation : public testing::Test {
-   public:
-    void SetUp() override {
-        pfakeGyro = std::unique_ptr<FakeGyro>(new FakeGyro());
-    }
-
-    void TearDown() override {}
-};
-
-TEST_F(UT_Rotation, positive) {
-    pfakeGyro->set_gyro(100, 200, 300);
-    rotation_mdegps moved_angle = get_rotation(&fake_gyro_wrapper);
+TEST(UT_Rotation, positive) {
+    auto gyro_fake = [](float* xyz) {
+        xyz[0] = 100;
+        xyz[1] = 200;
+        xyz[2] = 300;
+    };
+    rotation_mdegps moved_angle = get_rotation(gyro_fake);
     EXPECT_EQ(100, moved_angle.x);
     EXPECT_EQ(200, moved_angle.y);
     EXPECT_EQ(300, moved_angle.z);
--- blinky/test/movement/movement_test.cpp
+++ blinky/test/movement/movement_test.cpp
@@ -2,48 +2,50 @@ extern "C" {
 #include "movement.h"
 }
 #include "gtest/gtest.h"
-#include "internal/fake_gyro.hpp"
 using namespace ::testing;
 
-// Wrapper function uses a global to remember the object:
-static std::unique_ptr<FakeGyro> pfakeGyro;
-static void fake_gyro_wrapper(float* xyz) { pfakeGyro->gyro_xyz(xyz); }
-
-class UT_Movement : public testing::Test {
-   public:
-    void SetUp() override {
-        pfakeGyro = std::unique_ptr<FakeGyro>(new FakeGyro());
-    }
-
-    void TearDown() override {}
-};
-
-TEST_F(UT_Movement, run_over_threshold) {
-    pfakeGyro->set_gyro(100, 200, 10001);
-    movement_init(&fake_gyro_wrapper);
+TEST(UT_Movement, run_over_threshold) {
+    auto gyro_fake = [](float *xyz) {
+        xyz[0] = 100;
+        xyz[1] = 200;
+        xyz[2] = 10001;
+    };
+    movement_init(gyro_fake);
     movement_reset();
     movement_run();
     EXPECT_TRUE(movement_has_rotated());
 }
 
-TEST_F(UT_Movement, run_below_threshold) {
-    pfakeGyro->set_gyro(10000, 200, 400);
-    movement_init(&fake_gyro_wrapper);
+TEST(UT_Movement, run_below_threshold) {
+    auto gyro_fake = [](float *xyz) {
+        xyz[0] = 10000;
+        xyz[1] = 200;
+        xyz[2] = 400;
+    };
+    movement_init(gyro_fake);
     movement_reset();
     movement_run();
     EXPECT_FALSE(movement_has_rotated());
 }
 
-TEST_F(UT_Movement, not_run_over_threshold) {
-    pfakeGyro->set_gyro(100, 200, 10001);
-    movement_init(&fake_gyro_wrapper);
+TEST(UT_Movement, not_run_over_threshold) {
+    auto gyro_fake = [](float *xyz) {
+        xyz[0] = 100;
+        xyz[1] = 200;
+        xyz[2] = 10001;
+    };
+    movement_init(gyro_fake);
     movement_reset();
     EXPECT_FALSE(movement_has_rotated());
 }
 
-TEST_F(UT_Movement, reset) {
-    pfakeGyro->set_gyro(100, 200, 10001);
-    movement_init(&fake_gyro_wrapper);
+TEST(UT_Movement, reset) {
+    auto gyro_fake = [](float *xyz) {
+        xyz[0] = 100;
+        xyz[1] = 200;
+        xyz[2] = 10001;
+    };
+    movement_init(gyro_fake);
     movement_reset();
     EXPECT_FALSE(movement_has_rotated());
     movement_run();
