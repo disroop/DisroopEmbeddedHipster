f2b9637e05a0d99b3268bf721a52b9b37619f32c
Stefan Eicher <stefan.eicher@gmail.com>
2021-07-04 17:51:29 +0200

    update test notes

--- blinky/test/blinky/blinky_test.cpp
+++ blinky/test/blinky/blinky_test.cpp
@@ -11,6 +11,13 @@ class UT_Blinky : public testing::Test {
     void TearDown() override {}
 };
 
+// Test notes
+// The blinky application shall detect a movement.
+// A movement detection shall be indicated with a led.
+// The led shall run for 2 seconds longer than the movement has been detected.
+// after two seconds of no movement detection the led shall be switched off.
+// a movement ist detected if the device rotated at as speed above n degrees/per second. How much is n?
+
 TEST_F(UT_Blinky, test1) {
     // int ledID = 4;
     // indication indicator =
