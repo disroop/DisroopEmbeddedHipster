57972998ae96f1aaafe18291dfff1ef3a5737ae9
Michel Meyer <meyer_michel@outlook.com>
2021-07-05 19:19:41 +0000

    add lambda with captures

--- blinky/test/indication/indication_test.cpp
+++ blinky/test/indication/indication_test.cpp
@@ -1,39 +1,50 @@
 extern "C" {
 #include "indication.h"
 }
-#include "fake_led.hpp"
 #include "gtest/gtest.h"
 
-// Wrapper function uses a global to remember the object:
-static std::unique_ptr<FakeLed> pfakeLed;
-static void fake_led_set(int id) { pfakeLed->setLed(id); }
-static void fake_led_disable(int id) { pfakeLed->disableLed(id); }
+std::function<void(int)> set_led, disable_led;
+extern "C" void wrapper_set_led(int i) { set_led(i); }
+extern "C" void wrapper_disable_led(int i) { disable_led(i); }
 
-class UT_Indication : public testing::Test {
-   public:
-    void SetUp() override {
-        pfakeLed = std::unique_ptr<FakeLed>(new FakeLed());
-    }
+TEST(UT_Indication, indicate) {
+    int ledId = 4;
+    int hwLedId = 0;
+    bool hwLedOn = false;
+    bool runDisableLed = false;
 
-    void TearDown() override {}
-};
+    set_led = [&](int id) {
+        hwLedId = id;
+        hwLedOn = true;
+    };
+    disable_led = [&](int id) { runDisableLed = true; };
 
-TEST_F(UT_Indication, indicate) {
-    int ledID = 4;
     indication indicator =
-        indication_create(ledID, &fake_led_set, &fake_led_disable);
+        indication_create(ledId, wrapper_set_led, wrapper_disable_led);
+
     indication_indicate(indicator);
-    EXPECT_EQ(ledID, pfakeLed->getId());
-    EXPECT_TRUE(pfakeLed->isSet());
+    EXPECT_EQ(ledId, hwLedId);
+    EXPECT_TRUE(hwLedOn);
+    EXPECT_FALSE(runDisableLed);
 }
 
-TEST_F(UT_Indication, reset_after_set) {
-    int ledID = 4;
+TEST(UT_Indication, reset_led) {
+    int ledId = 4;
+    int hwLedId = 0;
+    bool hwLedOn = false;
+    bool runEnableLed = false;
+
+    set_led = [&](int id) { runEnableLed = true; };
+    disable_led = [&](int id) {
+        hwLedId = id;
+        hwLedOn = false;
+    };
+
     indication indicator =
-        indication_create(ledID, &fake_led_set, &fake_led_disable);
-    indication_indicate(indicator);
-    EXPECT_EQ(ledID, pfakeLed->getId());
-    EXPECT_TRUE(pfakeLed->isSet());
+        indication_create(ledId, wrapper_set_led, wrapper_disable_led);
+
     indication_reset(indicator);
-    EXPECT_FALSE(pfakeLed->isSet());
+    EXPECT_EQ(ledId, hwLedId);
+    EXPECT_FALSE(hwLedOn);
+    EXPECT_FALSE(runEnableLed);
 }
