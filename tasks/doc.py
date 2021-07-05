d3c05be33c5f8762da621f54222c05904ec8eb7f
Stefan Eicher <stefan.eicher@gmail.com>
2021-07-05 23:54:47 +0200

    update doc

--- tasks/doc.py
+++ tasks/doc.py
@@ -10,6 +10,7 @@ def gen(c):
     c.run("inv -l code> ./doc/vscode_commands.txt")
 
     # conan graphs
-    c.run("conan info ./blinky --graph doc/conan_graph.dot")
+    c.run("conan info ./blinky --graph doc/conan_graph_utest.dot")
+    c.run("conan info ./blinky --graph doc/conan_graph_blinky_debug.dot -pr=.profiles/arm-m4-debug")
     # cmakegraphs
     # todo
