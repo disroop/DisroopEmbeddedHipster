48c55c83dc9d511328b3ab56c7d15c825111ddea
Michel Meyer <meyer_michel@outlook.com>
2021-07-11 10:29:57 +0000

    update blinky doc

--- tasks/doc.py
+++ tasks/doc.py
@@ -1,5 +1,19 @@
 from invoke import task
+import shutil
+import os
 
+def _generate_blinky_conan_graph(context, output_folder: str, file_name: str, build_arg: str = ""):
+    context.run(f"conan info ./blinky --graph {output_folder}/{file_name}.dot {build_arg}")
+    context.run(f"dot -Tpng {output_folder}/{file_name}.dot > {output_folder}/{file_name}.png")
+    os.remove(f"{output_folder}/{file_name}.dot")
+
+
+def _generate_blinky_cmake_graph(context, output_folder: str, file_name: str, build_folder: str,  build_arg: str):
+    context.run(f"conan install  ./blinky  --install-folder={build_folder} {build_arg}")
+    os.mkdir(f"{output_folder}/tmp")
+    context.run(f'bash -c "source {build_folder}/activate.sh && cmake -S ./blinky -B {build_folder} --graphviz={output_folder}/tmp/cmake-target-graph.dot"')
+    context.run(f"dot -Tpng {output_folder}/tmp/cmake-target-graph.dot > {output_folder}/{file_name}.png")
+    shutil.rmtree(f"{output_folder}/tmp")
 
 @task(aliases=['g'])
 def gen(c):
@@ -10,7 +24,12 @@ def gen(c):
     c.run("inv -l code> ./doc/vscode_commands.txt")
 
     # conan graphs
-    c.run("conan info ./blinky --graph doc/conan_graph_utest.dot")
-    c.run("conan info ./blinky --graph doc/conan_graph_blinky_debug.dot -pr=.profiles/arm-m4-debug")
+    _generate_blinky_conan_graph(c, "blinky/assets/doc-images", "conan_graph_utest")
+    _generate_blinky_conan_graph(c, "blinky/assets/doc-images", "conan_graph_blinky", "-pr:h=.profiles/arm-m4-release -pr:b=default")
+
     # cmakegraphs
     # todo
+    _generate_blinky_cmake_graph(c, output_folder="blinky/assets/doc-images", file_name="cmake_blinky_utest", build_folder="build/utest",  build_arg="--profile:build=default --profile:host=default --build missing -s:h build_type=Debug")
+    _generate_blinky_cmake_graph(c, output_folder="blinky/assets/doc-images", file_name="cmake_blinky", build_folder="build/blinky-release",  build_arg="--profile:build=default --profile:host=/workspace/.profiles/arm-m4-release --build missing")
+    
+    
