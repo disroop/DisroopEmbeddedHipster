0444b60c4aaf129ddb064c832b975d47e7909d96
Michel Meyer <meyer_michel@outlook.com>
2021-10-01 05:24:20 +0000

    update conan version

--- build.py
+++ build.py
@@ -33,7 +33,7 @@ def build_blinky(c):
 
 @task(aliases=['bbc'])
 def build_blinky_in_container(c):
-    run_in_docker("disroop/embedded-hipster:0.6.12", "invoke build-blinky")
+    run_in_docker("disroop/embedded-hipster:0.6.13", "invoke build-blinky")
 
 
 namespace = Collection(coverage,
